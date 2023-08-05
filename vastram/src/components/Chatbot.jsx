b8204965ccb90ca9858c7d96bc906de1b003ab36
pankaj <pnkjvshsh1@gmail.com>
2023-08-05 12:09:41 +0530

    done

--- vastram/src/components/Chatbot.jsx
+++ vastram/src/components/Chatbot.jsx
@@ -34,7 +34,7 @@ export const Chatbot = () => {
         {
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${process.env.openai}`,
+            Authorization: `Bearer ${process.env.REACT_APP_SECRET_CODE}`,
           },
         }
       );
